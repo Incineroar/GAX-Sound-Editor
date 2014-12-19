09c6f27a3e6284dd2b771cd3c08d5e8dc0ca0f4b
Team Fail <seedbby@gmail.com>
2014-12-19 15:13:02 -0700

    Initial Commit
    First commit! Yay! Everything is probably broken, just saying.

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace GAX_Sound_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDataBlock = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSampleTable = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUnknown = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(157, 342);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Beginning of Data Block";
+            // 
+            // txtDataBlock
+            // 
+            this.txtDataBlock.Location = new System.Drawing.Point(180, 56);
+            this.txtDataBlock.Name = "txtDataBlock";
+            this.txtDataBlock.Size = new System.Drawing.Size(114, 20);
+            this.txtDataBlock.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Song Sample Table";
+            // 
+            // txtSampleTable
+            // 
+            this.txtSampleTable.Location = new System.Drawing.Point(180, 100);
+            this.txtSampleTable.Name = "txtSampleTable";
+            this.txtSampleTable.Size = new System.Drawing.Size(114, 20);
+            this.txtSampleTable.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(177, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Unknown";
+            // 
+            // txtUnknown
+            // 
+            this.txtUnknown.Location = new System.Drawing.Point(180, 139);
+            this.txtUnknown.Name = "txtUnknown";
+            this.txtUnknown.Size = new System.Drawing.Size(114, 20);
+            this.txtUnknown.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(302, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(321, 368);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 389);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtUnknown);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSampleTable);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDataBlock);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDataBlock;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSampleTable;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUnknown;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Ini;
+
+namespace GAX_Sound_Editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string fileLocation;
+        List<string> soundTable = new List<string>();
+        DataTable dt = new DataTable();
+        string gameCode;
+        IniFile gameConfig = new IniFile(Application.StartupPath + "/games.ini");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(Application.StartupPath + "/games.ini") == false)
+            {
+                MessageBox.Show("Games INI file not found! Cannot continue!");
+                return;
+            }
+            else
+            {
+                dataGridView1.DataSource = dt;
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    fileLocation = ofd.FileName;
+                    BinaryReader br = new BinaryReader(new FileStream(fileLocation, FileMode.Open));
+                    br.BaseStream.Seek(0xAC, SeekOrigin.Begin);
+                    gameCode = Encoding.UTF8.GetString(br.ReadBytes(4));
+                    br.Close();
+                    switch (gameConfig.IniReadValue(gameCode, "LoadFormat"))
+                    {
+                        case "0": //Case where audio table is known.
+                            {
+                                soundTable.Clear();
+                                long tableOffset = long.Parse(gameConfig.IniReadValue(gameCode, "TableOffset"), System.Globalization.NumberStyles.HexNumber);
+                                loadAudioTable(fileLocation, tableOffset);
+                                break;
+                            }
+                        case "1":
+                            {
+                                soundTable.Clear();
+                                loadTableFromINI();
+                                break;
+                            }
+                        default:
+                            {
+                                listBox1.Items.Clear();
+                                MessageBox.Show("Either there is no LoadFormat configuration for this particular game, or this game does not exist in the configuration! Cannot continue!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                break;
+                            }
+                    }
+
+                    listBox1.Items.Clear();
+
+                    foreach (string s in soundTable)
+                    {
+                        listBox1.Items.Add(s);
+                    }
+                }
+            }
+        }
+
+        private void loadAudioTable(string fileLocation, long tableOffset)
+        {
+            bool isValid = true;
+            FileStream fs = new FileStream(fileLocation, FileMode.Open);
+            fs.Seek(tableOffset, SeekOrigin.Begin);
+            while (isValid)
+            {
+                byte[] tempBuffer = new byte[4];
+                fs.Read(tempBuffer, 0, tempBuffer.Length);
+                if (tempBuffer[3] == 8)
+                {
+                    soundTable.Add(tempBuffer[2].ToString("X2") + tempBuffer[1].ToString("X2") + tempBuffer[0].ToString("X2"));
+                }
+                else
+                    isValid = false;
+            }
+            fs.Close();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dt.Reset();
+            dataGridView1.Refresh();
+            int tableCount = 1;
+            bool isValid = true;
+            FileStream fs = new FileStream(fileLocation, FileMode.Open);
+            fs.Seek(long.Parse(listBox1.SelectedItem.ToString(), System.Globalization.NumberStyles.HexNumber) + 0xC, SeekOrigin.Begin);
+            byte[] tmp = new byte[4];
+            fs.Read(tmp, 0, tmp.Length);
+            txtDataBlock.Text = "0x" + tmp[2].ToString("X2") + tmp[1].ToString("X2") + tmp[0].ToString("X2");
+            fs.Read(tmp, 0, tmp.Length);
+            txtSampleTable.Text = "0x" + tmp[2].ToString("X2") + tmp[1].ToString("X2") + tmp[0].ToString("X2");
+            fs.Read(tmp, 0, tmp.Length);
+            if (tmp[3] == 0x08)
+            {
+                txtUnknown.Text = "0x" + tmp[2].ToString("X2") + tmp[1].ToString("X2") + tmp[0].ToString("X2");
+                fs.Seek(0x08, SeekOrigin.Current);
+            }
+            else
+            {
+                txtUnknown.Text = "Not present.";
+                fs.Seek(0x04, SeekOrigin.Current);
+            }
+            
+
+            while (isValid)
+            {
+
+                byte[] tempBuffer = new byte[4];
+                fs.Read(tempBuffer, 0, tempBuffer.Length);
+                if (tempBuffer[3] == 8)
+                {
+                    dt.Columns.Add("Channel " + tableCount.ToString() + " - 0x" + tempBuffer[2].ToString("X2") + tempBuffer[1].ToString("X2") + tempBuffer[0].ToString("X2"));
+                    tableCount++;
+                }
+                else
+                    isValid = false;
+            }
+            fs.Close();
+        }
+
+        private void loadTableFromINI()
+        {
+            bool isNotNull = true;
+            int i = 1;
+            while (isNotNull)
+            {
+                if (gameConfig.IniReadValue(gameCode, "Song" + i.ToString()) != "")
+                {
+                    soundTable.Add(gameConfig.IniReadValue(gameCode, "Song" + i.ToString()));
+                    i++;
+                }
+                else
+                    break;
+            }
+        }
+
+        private void getReadFormat()
+        {
+            switch (gameConfig.IniReadValue(gameCode, "ReadFormat"))
+            {
+
+            }
+        }
+    }
+}
